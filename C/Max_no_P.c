ef5fa12707da475ec31fba7e406850f5087d016e
MukundPatidar <mukundpatidar6260@gmail.com>
2023-09-26 21:00:01 +0530

    new C

---
+++ C/Max_no_P.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void maximum_no(int *a,int *b);
+
+int main ()
+{
+    int z;
+    do{
+        int a, b;
+        printf("enter first number : ");
+        sacnf("%d", &a);
+        printf("enter second number : ");
+        sacnf("%d", &b);
+
+        maximum_no(&a, &b);
+        printf("%d is grater then %d", a, b);
+
+        printf("\nYou want to repeat \nif YES then type '1' if NO then type '0' :");
+        scanf("%d",&z);
+    } while (z == 1);
+
+    return 0;
+}
+
+void maximum_no(int *a,int *b){
+    if(*a > *b){
+    } else if(*b > *a){
+        int t = *a;
+            *a = *b;
+            *b = t;
+    } else if(*a == *b){
+        printf("%d and %d is equal", *a, *b);
+    }
+}
