5cff0eb48ae9db4716f2bba49c78f73b3069d895
MukundPatidar <mukundpatidar6260@gmail.com>
2023-11-02 08:48:26 +0530

    new c

--- C/Calculator.c
+++ C/Calculator.c
@@ -2,8 +2,46 @@
 
 int main()
 {
-    float a, b;
+    float num1, num2, ans;
+    char operator;
 
     printf("Enter first value : ");
-    scanf
+    scanf("%f",&num1);
+
+    printf("Enter the operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter second value : ");
+    scanf("%f",&num2);
+
+    switch (operator)
+    {
+    case '+' :
+        ans = num1 + num2;
+        break;
+    
+    case '-' :
+        ans = num1 - num2;
+        break;
+
+    case '*' :
+        ans = num1 * num2;
+        break;
+
+    case '/' :
+        if(num2 != 0){
+            ans = num1 / num2;
+        } else {
+            printf("Error: Cannot divide by zero!");
+        }
+        break;
+    
+    default:
+        printf("Error: Invalid operator!");
+        break;
+    }
+
+    printf("result : %.2f %c %.2f = %.2f",num1, operator, num2, ans);
+
+    return 0;
 }
