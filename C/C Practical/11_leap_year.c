b607e440beb7fadaed95ac961b72b9ffce2a9eca
MukundPatidar <mukundpatidar6260@gmail.com>
2023-12-05 18:03:33 +0530

    rebgeiy

--- C/C Practical/11_leap_year.c
+++ C/C Practical/11_leap_year.c
@@ -7,7 +7,7 @@ int main()
    printf("Enter a year: ");
    scanf("%d", &year);
 
-   if (year % 400 == 0 || year % 4 == 0) {
+   if (year % 400 == 0 && year % 100 != 0) {
       printf("%d is a leap year.", year);
    } else {
       printf("%d is not a leap year.", year);
