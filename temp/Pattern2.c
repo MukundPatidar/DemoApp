c9e12dc558a76ed38fc7337aac9a6d23af84efa8
MukundPatidar <mukundpatidar6260@gmail.com>
2023-12-18 09:14:57 +0530

    dadd

--- temp/Pattern2.c
+++ temp/Pattern2.c
@@ -2,7 +2,11 @@
 
 int main ()
 {
-    for (int i = 1; i < 0; i++) {
+    for (int i = 1; i <= 5; i++) {
+
+        for(int k = 4; k >= i; k--){
+            printf(" ");
+        }
 
         for(int j = 1; j <= i; j++){
             printf("%d ",j);
