666deece1b64ed77bdb28381b86eba03bd75edec
MukundPatidar <mukundpatidar6260@gmail.com>
2024-03-03 11:20:02 +0530

    casz

--- JAVA/Arrays1.java
+++ JAVA/Arrays1.java
@@ -9,20 +9,21 @@ public class Arrays1 {
         //Array size
         System.out.print("how many numbers entering : ");
         int size = sc.nextInt();
-
+        
         //Array
         int ace[] = new int[size];
-
+        
         //Array input
         for (i=0; i<ace.length; i++) {
             System.out.print("Enter element "+(i+1)+" : ");
             ace [i] = sc.nextInt();
         }
-
+        
+        sc.close();
         //Array sorte
         for (i=0; i<ace.length; i++) {
             for (j=i+1; j<ace.length; j++) {
-
+                
                 if (ace[i] > ace[j]) {
                     temp = ace[i];
                     ace[i] = ace[j];
@@ -35,6 +36,5 @@ public class Arrays1 {
         for (i=0; i<ace.length; i++) {
             System.out.print(ace[i] + ", ");
         }
-        sc.close();
     }
 }
