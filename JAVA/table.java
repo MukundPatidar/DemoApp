b6370e1937e521cc6ea194f3ef99622d8a36a284
MukundPatidar <mukundpatidar6260@gmail.com>
2024-05-07 07:50:04 +0530

    gsg

--- JAVA/table.java
+++ JAVA/table.java
@@ -1,17 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class table {
     public static void main(String args[]) {
       Scanner sc = new Scanner(System.in) ;
-      int a, i, input ;
+      int a, i, x ;
 
       do{
-          a = sc.nextInt();
+        System.out.print("Enter a number: ");
+        a = sc.nextInt();
+
         for(i=1; i<=10; i++) {
           System.out.printf("%d * %d = %d \n", a, i, a * i);
         }
+
         System.out.println("want to continue ? (Yes(1) or No(0))");
-          input = sc.nextInt();
+        x = sc.nextInt();
           
-        } while(input == 1);
+        } while(x == 1);
     }
 }
