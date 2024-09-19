7711617f816b2026f2baddb423533ed53dbc2440
MukundPatidar <mukundpatidar6260@gmail.com>
2024-09-19 13:34:50 +0530

    bbdfghbcx

---
+++ LeetCode/lengthOfLongestSubstring.java
@@ -0,0 +1,26 @@
+public class lengthOfLongestSubstring {
+    public static int length(String s) {
+        StringBuilder str = new StringBuilder();
+        boolean status = true;
+
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<str.length(); j++){
+                if(s.charAt(i) == str.charAt(j)){
+                    status = false;
+                    break;
+                }
+            }
+            if(status == true){
+                str.append(s.charAt(i));
+            }
+        }
+
+        return str.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(length("abcabcbb"));
+        System.out.println(length("bbbbb"));
+        System.out.println(length("pwwkew"));
+    }
+}
