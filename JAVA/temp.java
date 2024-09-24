13f4af30842f6b32eea88b8f4a9181466398fdb4
MukundPatidar <mukundpatidar6260@gmail.com>
2024-09-24 18:21:12 +0530

    jzxbvkjdjnsfv

--- JAVA/temp.java
+++ JAVA/temp.java
@@ -1,32 +1,33 @@
 public class temp {
     public static int reverse(int x) {
-        if(2147483647 < (x*10) || -2147483648 > (x*10)){
-            return 0;
-        }
-
         boolean s = true;
-        int n = 0;
+        long n = 0;
         if(x < 0){
             x *= -1;
             s = false;
         }
-        System.out.println("x is "+x);
+        
         while(x > 0){
             int temp = x % 10;
-            System.out.println("temp is "+ temp);
+            if(n*10 > 2147483647){
+                return 0;
+            }
             n *= 10;
             n += temp;
-            System.out.println("n is "+ n);
             x /= 10;
         }
+
 
         if(s == false){
             n *= -1;
         }
 
-        return n;
+        return (int)n;
     }
     public static void main(String[] args) {
         System.out.println(reverse(1534236469));
+        // if(reverse(reverse(-2147483412)) == -2147483412){
+        //     System.out.println("Done");
+        // }
     }
 }
