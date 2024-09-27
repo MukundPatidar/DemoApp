c62519bcc223367cb66f534edf5b5f2de2853eaa
MukundPatidar <mukundpatidar6260@gmail.com>
2024-09-27 15:52:44 +0530

    sgsg

--- JAVA/temp.java
+++ JAVA/temp.java
@@ -1,33 +1,3 @@
 public class temp {
-    public static int reverse(int x) {
-        boolean s = true;
-        long n = 0;
-        if(x < 0){
-            x *= -1;
-            s = false;
-        }
-        
-        while(x > 0){
-            int temp = x % 10;
-            if(n*10 > 2147483647){
-                return 0;
-            }
-            n *= 10;
-            n += temp;
-            x /= 10;
-        }
-
-
-        if(s == false){
-            n *= -1;
-        }
-
-        return (int)n;
-    }
-    public static void main(String[] args) {
-        System.out.println(reverse(1534236469));
-        // if(reverse(reverse(-2147483412)) == -2147483412){
-        //     System.out.println("Done");
-        // }
-    }
+    
 }
