eda72112c9ddd789d5b298d62c6a3eaea208e444
MukundPatidar <mukundpatidar6260@gmail.com>
2024-10-20 18:31:55 +0530

    hglhg

--- JAVA/LeetCode/BestTimeToBuySellStockII.java
+++ JAVA/LeetCode/BestTimeToBuySellStockII.java
@@ -1,15 +1,15 @@
 package LeetCode;
 
 public class BestTimeToBuySellStockII {
-    public int maxProfit(int[] prices) {
-        int profit = 0;
+	public int maxProfit(int[] prices) {
+		int profit = 0;
 
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i] < prices[i + 1]) {
-                profit += prices[i + 1] - prices[i];
-            }
-        }
+		for (int i = 0; i < prices.length - 1; i++) {
+			if (prices[i] < prices[i + 1]) {
+				profit += prices[i + 1] - prices[i];
+			}
+		}
 
-        return profit;
-    }
+		return profit;
+	}
 }
