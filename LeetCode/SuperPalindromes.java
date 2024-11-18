62eac7d43899d1f5afdb924ab76acc8c32b8ad05
MukundPatidar <mukundpatidar6260@gmail.com>
2024-11-18 22:05:46 +0530

    hvjvf

--- LeetCode/SuperPalindromes.java
+++ LeetCode/SuperPalindromes.java
@@ -2,26 +2,30 @@ package LeetCode;
 
 public class SuperPalindromes {
 	public static void main(String[] args) {
-		System.out.println(superpalindromesInRange("40000000000000000", "50000000000000000"));
+		// System.out.println(superpalindromesInRange("40000000000000000", "50000000000000000"));
+		// String str = String.format("%.0f", 50000000000000000.541645);
+		double d = 50000000000000000.0;
+		double d1 = 50000000000000000.5465445;
+		System.out.println(Math.abs(d % 1) > 1e-10);
+		System.out.println(Math.abs(d1 % 1) > 1e-10);
 	}
 
 	public static int superpalindromesInRange(String left, String right) {
 		int ans = 0;
 
-		Long leftNum = Long.parseLong(left);
-		Long rightNum = Long.parseLong(right);
+		double leftNum = Math.floor(Math.sqrt(Long.parseLong(left)));
+		double rightNum = Math.ceil(Math.sqrt(Long.parseLong(right)));
 
-		for (Long i = leftNum; i <= rightNum; i++) {
-			String num = Long.toString(i);
+		for (double i = leftNum; i <= rightNum; i++) {
+			if (((i * i) != (long) (i * i))) {
+				System.out.println(i);
+				continue;
+			}
 
-			if (isPalindrome(num)) {
-				Double temp = Math.sqrt(i);
-				String sqrt = Double.toString(temp);
-
-				if (isPalindrome(sqrt)) {
+			if (isPalindrome(Double.toString(i))) {
+				if (isPalindrome(Double.toString(i * i))) {
 					ans++;
 				}
-
 			}
 		}
 
@@ -29,20 +33,14 @@ public class SuperPalindromes {
 	}
 
 	public static boolean isPalindrome(String s) {
-		int left = 0;
-		int right = 0;
-
-		boolean status = false;
-		for (int i = 0; i < s.length(); i++) {
-			if (status && s.charAt(i) != '0') {
+		for (int i = s.indexOf(".") + 1; i < s.length(); i++) {
+			if (s.charAt(i) != '0') {
 				return false;
-			}
-			if (s.charAt(i) == '.') {
-				right = i - 1;
-				status = true;
 			}
 		}
 
+		int left = 0;
+		int right = s.indexOf(".") - 1;
 		while (left < right) {
 			if (s.charAt(right) != s.charAt(left)) {
 				return false;
