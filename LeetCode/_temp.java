6dd20a7729dd76886b84dae0fd59c881bee5fe69
MukundPatidar <mukundpatidar6260@gmail.com>
2024-12-14 12:41:46 +0530

    xfcbd

--- LeetCode/_temp.java
+++ LeetCode/_temp.java
@@ -1,24 +1,6 @@
 package LeetCode;
 
-<<<<<<< HEAD
-import java.util.Arrays;
-
 public class _temp {
 	public static void main(String[] args) {
-		int[][] nums = { { 1, 4 }, { 3, 2 }, { 5, 3 }, { 4, 1 }, { 2, 3 }, { 1, 1 } };
-		System.out.println(Arrays.deepToString(nums));
-		Arrays.sort(nums, (a, b) -> {
-			if (a[0] != b[0]) {
-				return Integer.compare(a[0], b[0]);
-			} else {
-				return Integer.compare(a[1], b[1]);
-			}
-		});
-		System.out.println(Arrays.deepToString(nums));
-=======
-public class _temp {
-	public static void main(String[] args) {
-		System.out.println(2);
->>>>>>> 2d4b2833c148e007a78ddf346c7195ce7c166e54
 	}
 }
